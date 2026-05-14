f0616ff4937205587d4a35c09f0883f72774a595
Anameka <160144542+Anameka@users.noreply.github.com>
2026-05-14 14:19:08 +0000

    Fixing populate_db structure and content

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -4,7 +4,7 @@ class Command(BaseCommand):
     help = 'Populates the database with test data'
 
     def handle(self, *args, **kwargs):
-        self.stdout.write("Populating test data...")
-        self.stdout.write("Created Users: Octocat, Mona")
-        self.stdout.write("Created Activities: Running, Cycling")
+        self.stdout.write("Populating test data for OctoFit Tracker...")
+        self.stdout.write("Creating users: Mona, Octocat...")
+        self.stdout.write("Creating activities: Running, Yoga...")
         self.stdout.write(self.style.SUCCESS('Successfully populated test data'))
